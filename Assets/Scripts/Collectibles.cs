916119323a0f6750d605f493b0d494cd0dccb860
Minh TrÂ¿n <trnmi822@student.otago.ac.nz>
2020-02-01 22:12:41 +1300

    Add crude knife. Need to: Put Kinfe Icon to the GUI, Smoothen the connection between has Knife and references.

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Collectibles : MonoBehaviour
+{
+    public GameObject knifeIcon;
+    public static bool hasKnife = false;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (knifeIcon != null && gameObject.tag == "Knife")
+            {
+                knifeIcon.SetActive(true);
+                hasKnife = true;
+            } else if (knifeIcon == null)
+            {
+                Debug.Log("That piece of shit icon is not found");
+            }
+            Destroy(gameObject);
+        }
+    }
+}
