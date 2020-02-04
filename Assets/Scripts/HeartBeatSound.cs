44cf20e6a44920ba225e7058aad39b15af8fbb90
Elshtein99 <57881656+Elshtein99@users.noreply.github.com>
2020-02-04 21:38:03 +1300

    Heart Beat Progress

---
+++ Assets/Scripts/HeartBeatSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBeatSound : MonoBehaviour
+{
+
+    public AudioSource heartBeat_Whole;
+    public AudioSource heartBeat_First;
+    public AudioSource heartBeat_Last;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heartBeat_Whole.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
