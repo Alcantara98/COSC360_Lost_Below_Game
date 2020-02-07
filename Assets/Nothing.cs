7f68c6b43c08fc0cc27ab1ce15d439f2219c3ce7
Elshtein99 <57881656+Elshtein99@users.noreply.github.com>
2020-02-07 21:38:03 +1300

    Added Idle Animation for player

--- Assets/Nothing.cs
+++ Assets/Nothing.cs
@@ -1,6 +1,1 @@
-﻿https://nielson.io/2015/12/splatter-effects-in-unity-using-the-stencil-buffer
-https://www.ronja-tutorials.com/2018/08/18/stencil-buffers.html
-https://answers.unity.com/questions/316064/can-i-obscure-an-object-using-an-invisible-object.html?_ga=2.116652998.1153920511.1580807356-1000541768.1579571915
-https://www.youtube.com/watch?v=l7LQmJuoiBY
-https://answers.unity.com/questions/1183400/hiding-objectspart-of-objects-behind-a-invisible-w.html
-https://alastaira.wordpress.com/2014/12/27/using-the-stencil-buffer-in-unity-free/
+﻿
