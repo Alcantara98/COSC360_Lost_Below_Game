e1b942203a65b9ef316205f5f61104179f99420b
obaden <oscarbaden@protonmail.com>
2020-02-13 12:15:17 +1300

    no message

--- Assets/Scripts/PlayerCollectibles.cs
+++ Assets/Scripts/PlayerCollectibles.cs
@@ -54,7 +54,7 @@ public class PlayerCollectibles : MonoBehaviour
         if (collider.gameObject.tag == "Glow")
         {
             nglow++;
-            collider.gameObject.SetActive(true);
+            collider.gameObject.SetActive(false);
         }
     }
 
