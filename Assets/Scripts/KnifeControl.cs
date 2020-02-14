064b99c07af9beb1bb22bee95b2476a588865952
Minh TrÂ¿n <trnmi822@student.otago.ac.nz>
2020-02-14 15:07:49 +1300

    Main menu reset the GUI

---
+++ Assets/Scripts/KnifeControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeControl : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerCollectibles.hasKnife == false) this.transform.gameObject.SetActive(false);
+    }
+}
